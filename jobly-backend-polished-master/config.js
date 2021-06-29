746dab139584207935311b2be49dff72aa1bb1fc
Matt Lane <lane.matthew@gmail.com>
2021-06-29 14:15:39 -0700

    more flexible config file
    allows for config when postgres user has no password set

--- jobly-backend-polished-master/config.js
+++ jobly-backend-polished-master/config.js
@@ -12,7 +12,7 @@ const PORT = +process.env.PORT || 3001
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
   const dbUser = process.env.DATABASE_USER || "postgres"
-  const dbPass = process.env.DATABASE_PASS ? encodeURI(process.env.DATABASE_PASS) : "postgres"
+  const dbPass = "DATABASE_PASS" in process.env ? encodeURI(process.env.DATABASE_PASS) : "postgres"
   const dbHost = process.env.DATABASE_HOST || "local"
   const dbPort = process.env.DATABASE_PORT || 5432
   // const dbName = process.env.NODE_ENV === "test" ? "jobly_test" : process.env.DATABASE_NAME || "jobly"
